4395c9a327f6585079099eba8d5488ce684aeeb7
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-02-08 18:31:47 +0100

    major changes: finished op. and res. for RQ1 and RQ2, maybe add patterns for RQ2 later

--- fse_22/linalg.py
+++ fse_22/linalg.py
@@ -6,8 +6,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.linear_model import LinearRegression, ElasticNet, Lasso
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, PolynomialFeatures
+
 from pycosa.util import remove_multicollinearity, get_vif
 from pycosa.plotting import mirrored_histogram
 import scipy.cluster.hierarchy as spc
@@ -17,13 +18,15 @@ from matplotlib.colors import ListedColormap
 import networkx as nx
 import logging
 import math
+import itertools
 import numpy as np
+from scipy.stats import ks_2samp, pearsonr, kendalltau, wilcoxon
 import matplotlib.cm as cm
+from cliffs_delta import cliffs_delta
 
 from matplotlib import gridspec
 
 import sys
-
 
 data = pd.read_csv('datax/h2_measurements.csv')
 da = data[data['workload'] == 'tpcc-2']['throughput']
@@ -67,7 +70,7 @@ for system in systems:
             
             for xcol in X.columns:
               X[xcol] = MinMaxScaler().fit_transform(X[xcol].values.reshape(-1, 1))
-                
+            
             ss = StandardScaler()
             y = ss.fit_transform(df[kpi].values.reshape(-1, 1))
 
@@ -98,14 +101,7 @@ for system in systems:
                 influences.append(coefs[opt])
             means[opt] = np.median(influences)
             covs[opt] = np.std(influences) 
-            
-            lowerb, upperb = outlier_treatment(influences)
-            outliers = len(list(filter(lambda x: x < lowerb or x > upperb, influences)))
-            
-            if covs[opt] > STDEV_THRESHOLD:
-                nr_outlier[opt] = outliers
-            else:
-                nr_outlier[opt] = 0
+
                 
         pivot = dict(sorted(means.items(), key=lambda item: item[1], reverse=True))
 
@@ -123,7 +119,7 @@ for system in systems:
                 plt.axhline(o, color='black', linewidth=0.4, alpha=0.25)
                 mi = swarm_coefficients[swarm_coefficients['option'] == o]['influence'].min()
                 ma = swarm_coefficients[swarm_coefficients['option'] == o]['influence'].max()
-                print(o, mi, ma)
+                #print(o, mi, ma)
                 plt.plot([mi, ma], [o, o], color='black',linewidth=1.4)
                 if mi < 0:
                     plt.barh([o], [mi], color='green', alpha=0.3)
@@ -134,114 +130,191 @@ for system in systems:
             plt.ylabel('')
             plt.draw()
             plt.savefig('{}_rq2.pdf'.format(system), bbox_inches='tight')
-        '''
-        workloads = swarm_coefficients.workload.unique()
-
-        cmap = plt.get_cmap('hsv')
-        colors = list([cmap(1.*i/len(workloads)) for i in range(len(workloads))])
-        colors = [list(c) for c in colors]
-        #for i in range(len(colors)):
-        #    colors[i][3] = 1.0
-        
-        
-        fig = plt.figure(figsize=(4.5, 4.5))
-        for j, o in enumerate(option_order):
-            plt.axhline(j, linewidth=0.3, alpha=0.5,color='black', linestyle=':')
-            #influences = np.random.laplace(0,3,size=len(workloads))
-            
-            influences = swarm_coefficients[swarm_coefficients['option'] == o].sort_values(by='workload')['influence'].values
-            
-            # diverse enough -> show full color spectrum
-            if True:
-                
-                median = np.median(influences)
-                std = np.std(influences)
-                
-                n_std = 2
-                indices = np.where( (influences < median - n_std*std) | (influences > median + n_std*std) )[0]
-                #print(indices)
-                colors_new = [[0.1,0.1,0.1,0.1] for i in range(len(colors))]
-                for i in indices:
-                    colors_new[i] = colors[i]
-            plt.scatter(influences, [o for i in range(len(workloads))], color=colors_new, s=22, marker='D')
-        plt.axvline(0, linewidth=0.4, alpha=0.5,color='black')
-        #plt.savefig('grafik.pdf', bbox_inches='tight')
-        #plt.clf()
-        plt.title('{} -- {}'.format(system, kpi))
-        plt.xlabel('Relative Performance Influence')
-        
-        legend_elements = []
-        
-        for i, w in enumerate(workloads):
-            legend_elements.append(
-                Line2D([0], [0], marker='D', color='w', label=w,
-                          markerfacecolor=colors[i], markersize=5))
-        fig.legend(handles=legend_elements, loc='lower center',ncol=3,bbox_to_anchor=(0.5, -0.3))
-        
-        #fig.subplots_adjust(bottom=0.25)
-        plt.show()
-        '''
-        
-        
-        '''
-        fig, ax = plt.subplots(nrows=1, ncols=2, sharex='col', sharey=True,
-                               gridspec_kw={'width_ratios': [3, 0.7]},
-                               figsize=(9, 6))
-        
-        
-        for z, coefs in enumerate(coefficients):
-            ax[0].plot(
-                [coefs[k] for k in option_order],
-                option_order,label=workloads[z], marker='.')
-         
-        ax[1].barh(list(option_order), [covs[opt] for opt in option_order], color='black', alpha=0.75)
-        #ax[2].barh(list(option_order), [nr_outlier[opt] for opt in option_order], color='brown', alpha=0.75)
-        
-        ax[0].legend(ncol=3, bbox_to_anchor=(0.8, -0.11))
-        plt.suptitle(system + ' -- ' + kpi)
-        #plt.xticks(rotation=45, rotation_mode='anchor')
-        ax[0].set_xlabel(r'Relative performance influence $ \beta_i$')
-        ax[0].axvline(0, linestyle='--', linewidth=1, color='grey')    
-        #ax[1].axvline(STDEV_THRESHOLD, linestyle='--', linewidth=1, color='brown')   
-        ax[1].set_xlabel('StDev $\sigma$')
-        #ax[2].set_xlabel('#Outliers exceeding\n threshold StDev')
-        #ax[2].set_xlim(0, len(workloads))
-        
-        plt.show()
-        '''
+            #plt.show()
 '''
-# Correlation Analysis
+# build elastic model
 for system in systems:
     data = pd.read_csv('datax/{}_measurements.csv'.format(system))
 
     for kpi in systems[system]:
-        workloads = data.workload.unique()
-        
-        df = pd.DataFrame()
-        for w in workloads:
-            df[w] = data[data['workload'] == w].sort_values(by='config')[kpi].values
+        if kpi not in ['time', 'throughput']:
+            continue
+        coefficients = []
+        swarm_coefficients = []
+        workloads = []
+        for w, df in data.groupby('workload'):
+            workloads.append(w)
+            X = df.drop(columns=['config', 'workload'] + systems[system])
+            X = remove_multicollinearity(X)
             
-        corr = df.corr(method='pearson')
-                
-        # Generate a mask for the upper triangle        
-        g = sns.clustermap(corr, cmap='RdBu_r', vmin=0, vmax=1.0, linewidth=3, 
-                           cbar_kws={ "orientation": "horizontal" },
-                           cbar_pos = (0.15, 0.83, 0.7, 0.01))
-        g.ax_heatmap.yaxis.set_ticks_position("left")
+            for xcol in X.columns:
+              X[xcol] = MinMaxScaler().fit_transform(X[xcol].values.reshape(-1, 1))
+            
+            poly = PolynomialFeatures(degree=2, interaction_only = True)
+            X_ = poly.fit_transform(X)
+            X = pd.DataFrame(X_, columns = poly.get_feature_names(X.columns))
+            X = remove_multicollinearity(X)
+            
+            print('here')
+            
+            ss = StandardScaler()
+            y = ss.fit_transform(df[kpi].values.reshape(-1, 1))
 
-        g.ax_row_dendrogram.set_visible(False)
-        g.ax_col_dendrogram.set_visible(False)
+            lm = Lasso()
+            lm.fit(X, y)
+            #print(lm.coef_)
+            coefs = {c: lm.coef_[i] for i, c in enumerate(X.columns)}
+            coefficients.append(coefs)
+            
+            for option, coefficient in coefs.items():
+                swarm_coefficients.append(
+                    {
+                        'workload': w,
+                        'option': option,
+                        'influence': coefficient
+                    }    
+                )
+            
+        # get worklaod with GREATEST absolute influence
+        options = coefficients[0].keys()
+        ginis = {}
+        covs = {}
+        means = {}
+        nr_outlier = {}
+        for opt in options:
+            influences = []
+            for coefs in coefficients:
+                influences.append(coefs[opt])
+            means[opt] = np.median(influences)
+            covs[opt] = np.std(influences) 
+
+                
+        pivot = dict(sorted(means.items(), key=lambda item: item[1], reverse=True))
+
+        option_order = reversed(pivot.keys())
+        option_order = [o for o in option_order]
+        swarm_coefficients = pd.DataFrame(swarm_coefficients)
         
-        plt.setp(g.ax_heatmap.xaxis.get_majorticklabels(), fontsize=10)
-        plt.setp(g.ax_heatmap.yaxis.get_majorticklabels(), fontsize=10)
+        for option in option_order:
+            minimum = swarm_coefficients[swarm_coefficients['option'] == option]['influence'].min()
+            maximum = swarm_coefficients[swarm_coefficients['option'] == option]['influence'].max()
+            
+            if abs(minimum) < 0.1 or abs(maximum) < 0.1:
+                swarm_coefficients = swarm_coefficients[swarm_coefficients['option'] != option]
         
-        mask = np.triu(np.ones_like(corr))
-        values = g.ax_heatmap.collections[0].get_array().reshape(corr.shape)
-        new_values = np.ma.array(values, mask=mask)
-        g.ax_heatmap.collections[0].set_array(new_values)
+        print(swarm_coefficients)
+        remaining = swarm_coefficients.option.unique()
+        option_order = list(filter(lambda o: o in remaining, option_order))
+        #plt.hist(swarm_coefficients['influence'], bins=100)
         
-        plt.title('{}_{}_kendall.eps'.format(system, kpi))
-        plt.show()
-        plt.savefig('grafix/{}_{}_kendall.eps'.format(system, kpi), bbox_inches='tight')
-        plt.clf()
-'''       
+        if kpi in ['time', 'throughput']:
+            #plt.figure(figsize=(8,4.5))
+            plt.grid(axis='y')
+            sns.stripplot(y="option", x="influence", data=swarm_coefficients, size=5,  order=option_order, marker='d', color='0.0')
+            #sns.boxplot(y="option", x="influence", data=swarm_coefficients, order=option_order, color='green', linewidth=0.8, boxprops={'alpha':0.7})
+            plt.axvline(0, color='black', linestyle='-', linewidth=0.5)
+            for i, o in enumerate(option_order):
+                plt.axhline(o, color='black', linewidth=0.4, alpha=0.25)
+                mi = swarm_coefficients[swarm_coefficients['option'] == o]['influence'].min()
+                ma = swarm_coefficients[swarm_coefficients['option'] == o]['influence'].max()
+
+                plt.plot([mi, ma], [o, o], color='black',linewidth=1.4)
+                if mi < 0:
+                    plt.barh([o], [mi], color='green', alpha=0.3)
+                if ma > 0:
+                    plt.barh([o], [ma], color='red', alpha=0.3)
+            plt.title('{} ({}; {} workloads)'.format(system, kpi, len(workloads)),fontweight='bold')
+            plt.xlabel('Relative Performance Influence')
+            plt.ylabel('')
+            #plt.draw()
+            #plt.savefig('{}_rq2.pdf'.format(system), bbox_inches='tight')
+            plt.show()
+'''
+
+def a12(lst1,lst2,rev=True):
+    "how often is x in lst1 more than y in lst2?"
+    more = same = 0.0
+    for x in lst1:
+        for y in lst2:
+            if   x==y : same += 1
+            elif rev     and x > y : more += 1
+            elif not rev and x < y : more += 1
+    return (more + 0.5*same)  / (len(lst1)*len(lst2))
+
+def distribution_analysis(dist1, dist2):
+    
+    # thresholds
+    t_kendall = 0.6
+    t_pearson = 0.6
+    t_ks = 0.05
+    t_a = (0.3, 0.7)
+    
+    # check for similar analysis
+    # kolmogorov smirnov
+    # A12 effect size
+    cliff, res = cliffs_delta(dist1, dist2)
+
+    test_statistic, p_value = wilcoxon(dist1, dist2)
+    
+    pearson = pearsonr(dist1, dist2)[0]
+    kendall = kendalltau(dist1, dist2)[0]
+    
+    categorization = np.zeros(4)
+    
+    # are they similar?
+    if p_value > t_ks and cliff > 0.147:
+        categorization[0] = 1
+    else:
+        categorization[0] = 0
+    
+    if np.abs(pearson) > t_pearson:
+        categorization[1] = 1
+    else:
+        categorization[1] = 0
+        
+    if np.abs(pearson) < t_pearson and np.abs(kendall) > t_kendall:
+        categorization[2] = 1
+    else:
+        categorization[2] = 0
+        
+    if np.abs(pearson) < t_pearson and np.abs(kendall) < t_kendall:
+        categorization[3] = 1
+    else:
+        categorization[3] = 0
+        
+    return categorization
+'''
+dist_a = np.random.laplace(0,1, size=500)
+dist_b = np.random.laplace(0,1, size=500)
+distribution_analysis(dist_a, dist_b)
+
+for system in systems:
+    data = pd.read_csv('datax/{}_measurements.csv'.format(system))
+
+    cats = []
+    for kpi in systems[system]:
+        if kpi in ['time', 'throughput']:
+            
+            workloads = data.workload.unique()
+            
+            for w1, w2 in itertools.combinations(workloads, 2):
+                d1 = data[data['workload'] == w1].sort_values(by='config')[kpi].values
+                d2 = data[data['workload'] == w2].sort_values(by='config')[kpi].values
+                
+                cat = distribution_analysis(d1, d2)
+                cats.append(cat)
+                
+    cats = np.vstack(cats)
+    plt.pcolormesh(cats)
+    plt.title(system)
+    plt.show()
+    
+    print(system, cats.shape[0])
+    print('SD', cats.sum(axis=0)[0], round(100*cats.sum(axis=0)[0] / cats.shape[0], 2))
+    print('LT', cats.sum(axis=0)[1], round(100*cats.sum(axis=0)[1] / cats.shape[0], 2))
+    print('XMT', cats.sum(axis=0)[2], round(100*cats.sum(axis=0)[2] / cats.shape[0], 2))
+    print('NMT', cats.sum(axis=0)[3], round(100*cats.sum(axis=0)[3] / cats.shape[0], 2))
+    print('----------')
+            
+            
+'''    
